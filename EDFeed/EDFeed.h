869096ec0b9a71070c56fab003432137e74b21c9
Alexey Koleda <alexeykoleda@icloud.com>
2023-01-03 16:56:04 +0300

    Initial Commit

---
+++ EDFeed/EDFeed.h
@@ -0,0 +1,18 @@
+//
+//  EDFeed.h
+//  EDFeed
+//
+//  Created by Alexey Koleda on 03.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EDFeed.
+FOUNDATION_EXPORT double EDFeedVersionNumber;
+
+//! Project version string for EDFeed.
+FOUNDATION_EXPORT const unsigned char EDFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDFeed/PublicHeader.h>
+
+
