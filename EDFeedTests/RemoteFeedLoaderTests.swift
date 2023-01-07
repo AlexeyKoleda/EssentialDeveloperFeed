6cbb051ce72bfb77cd6d029527862a5ebcb1c3d8
Alexey Koleda <alexeykoleda@icloud.com>
2023-01-07 17:58:07 +0300

    Add memory leaks detection

--- EDFeedTests/RemoteFeedLoaderTests.swift
+++ EDFeedTests/RemoteFeedLoaderTests.swift
@@ -97,9 +97,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     // MARK: Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(
+        url: URL = URL(string: "https://a-given-url.com")!,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        
         return (sut, client)
     }
     
