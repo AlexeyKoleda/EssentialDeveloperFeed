a1c11eae097f8b188db975d6db0f7931cfc62ea2
Alexey Koleda <alexeykoleda@icloud.com>
2023-01-17 12:41:49 +0300

    Add 'EDFeedCacheIntegrationTests' target to separate the potential-slower cache integration tests from the fast unit/isolated tests

---
+++ EDFeedCacheIntegrationTests/EDFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EDFeedCacheIntegrationTests.swift
+//  EDFeedCacheIntegrationTests
+//
+//  Created by Alexey Koleda on 17.01.2023.
+//
+
+import XCTest
+
+final class EDFeedCacheIntegrationTests: XCTestCase {
+
+
+}
